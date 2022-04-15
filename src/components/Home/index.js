7369ad4f004025cbe726ab1e2a6fc482478b89cd
Karam <karamguliyev10@gmail.com>
2022-04-15 22:56:53 +0300

    Building Landing Page

---
+++ src/components/Home/index.js
@@ -0,0 +1,39 @@
+import {  useState } from 'react'
+import { Link } from 'react-router-dom'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+
+const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = [' ', 'l', 'o', 'b', 'o', 'd', 'a', 'n']
+  const jobArray = ['w', 'e', 'b', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r', '.']
+
+  setTimeout(() => {setLetterClass('text-animate-hover')}, 4000)
+
+  return (
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _10`}>i</span>
+            <span className={`${letterClass} _11`}>,</span>
+            <br />
+            <span className={`${letterClass} _12`}>I</span>
+            <span className={`${letterClass} _13`}>' </span>
+            <span className={`${letterClass} _14`}>m </span>
+            <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15} />
+            <br />
+            <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={22} />
+          </h1>
+          <h2>Front End Developer</h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Home
