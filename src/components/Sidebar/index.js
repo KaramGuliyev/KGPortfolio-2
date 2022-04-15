36f249b435ee93bfe68923ead7372fc9d888a11a
Karam <karamguliyev10@gmail.com>
2022-04-15 20:36:02 +0300

    Adding Sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,6 +1,51 @@
 import './index.scss'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { Link, NavLink } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faInstagram, faLinkedin, faWhatsapp } from '@fortawesome/free-brands-svg-icons'
 
+const Sidebar = () => (
+  <div className="nav-bar">
+    <Link className="logo" to="/">
+      <img src={LogoS} alt="logo" />
+      <img className="sub_logo" src={LogoSubtitle} alt="slobodan" />
+    </Link>
+    <nav>
+      <NavLink exact="true" activeclassname="active" to="/">
+        <FontAwesomeIcon icon={faHome} color="#4d4d4d" />
+      </NavLink>
+      <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+        <FontAwesomeIcon icon={faUser} color="#4d4d4d" />
+      </NavLink>
+      <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4d" />
+      </NavLink>
+    </nav>
+    <ul>
+      <li>
+        <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/karamguliyev/">
+          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a target="_blank" rel="noreferrer" href="https://github.com/KaramGuliyev/">
+          <FontAwesomeIcon icon={faGithub} color="#4d4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a target="_blank" rel="noreferrer" href="https://instagram.com/karam_guliyev/">
+          <FontAwesomeIcon icon={faInstagram} color="#4d4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a target="_blank" rel="noreferrer" href="https://api.WhatsApp.com/send?phone=number">
+          <FontAwesomeIcon icon={faWhatsapp} color="#4d4d4d4e" />
+        </a>
+      </li>
+    </ul>
+  </div>
+)
 
-const Sidebar = () => <>Hello From sidebar</>
-
-export default Sidebar;
+export default Sidebar
